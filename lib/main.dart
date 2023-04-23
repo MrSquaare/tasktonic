b3bd61abadb8746001859eade9d4a606fc9d9b10
Guillaume Bonnet <mrsquaare@mrsquaare.fr>
2023-04-23 12:49:02 +0200

    feat: add showcase project
    - tweak default generated project

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,181 @@
+import 'package:android_intent_plus/android_intent.dart';
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:styled_widget/styled_widget.dart';
 
-void main() {
-  runApp(const MyApp());
+bool hasPerAppLocale(AndroidDeviceInfo? info) {
+  if (info == null) return false;
+
+  return info.version.sdkInt >= 33;
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+Future<AndroidDeviceInfo?> getAndroidInfo() async {
+  try {
+    final deviceInfo = DeviceInfoPlugin();
+    final androidInfo = await deviceInfo.androidInfo;
+
+    return androidInfo;
+  } catch (e) {
+    return null;
+  }
+}
+
+final androidInfoProvider = FutureProvider((ref) => getAndroidInfo());
+
+class CounterNotifier extends StateNotifier<int> {
+  CounterNotifier(int initialValue) : super(initialValue);
+
+  void increment() => state++;
+}
+
+final counterProvider = StateNotifierProvider(
+  (ref) => CounterNotifier(0),
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await EasyLocalization.ensureInitialized();
+
+  final androidInfo = await getAndroidInfo();
+  final perAppLocale = hasPerAppLocale(androidInfo);
+
+  runApp(
+    MyAppWrappers(
+      perAppLocale: perAppLocale,
+      child: LocaleListenerWidget(
+        child: MyApp(),
+      ),
+    ),
+  );
+}
+
+class MyAppWrappers extends StatelessWidget {
+  const MyAppWrappers({
+    super.key,
+    required this.child,
+    this.perAppLocale = false,
+    this.providerOverrides = const [],
+    this.providerObservers,
+  });
+
+  final Widget child;
+  final bool perAppLocale;
+  final List<Override> providerOverrides;
+  final List<ProviderObserver>? providerObservers;
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      overrides: providerOverrides,
+      observers: providerObservers,
+      child: EasyLocalization(
+        path: 'assets/translations',
+        supportedLocales: const [Locale('en'), Locale('fr')],
+        fallbackLocale: const Locale('en'),
+        saveLocale: perAppLocale == false,
+        child: child,
+      ),
+    );
+  }
+}
+
+class LocaleListenerWidget extends StatefulWidget {
+  const LocaleListenerWidget({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  State<LocaleListenerWidget> createState() => _LocaleListenerWidgetState();
+}
+
+class _LocaleListenerWidgetState extends State<LocaleListenerWidget>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeLocales(List<Locale>? locales) {
+    super.didChangeLocales(locales);
+
+    final locale = locales?.first;
+
+    if (locale != null) {
+      context.setLocale(Locale(locale.languageCode));
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
+
+class MyApp extends ConsumerWidget with WidgetsBindingObserver {
+  MyApp({super.key});
+
+  final GoRouter _router = GoRouter(
+    initialLocation: "/",
+    routes: [
+      ShellRoute(
+        builder: (context, state, child) {
+          return MyRootNavigationPage(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: "/",
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(
+                child: MyHomePage(),
+              );
+            },
+          ),
+          GoRoute(
+            path: "/settings",
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(
+                child: MySettingsPage(),
+              );
+            },
+            routes: [
+              GoRoute(
+                path: "language",
+                builder: (context, state) {
+                  return const MyLanguageSettingsPage();
+                },
+              ),
+            ],
+          ),
+        ],
+      )
+    ],
+  );
+
+  bool _isAppReady(WidgetRef ref) {
+    final androidInfo = ref.watch(androidInfoProvider);
+
+    return androidInfo.isLoading == false;
+  }
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isAppReady = _isAppReady(ref);
+
+    return MaterialApp.router(
+      title: 'TaskTonic',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,45 +188,70 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      routerConfig: _router,
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      builder: (context, child) {
+        return isAppReady ? child! : const MyLoadingPage();
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyLoadingPage extends StatelessWidget {
+  const MyLoadingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+class MyRootNavigationPage extends StatelessWidget {
+  const MyRootNavigationPage({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final state = GoRouterState.of(context);
+
+    return Scaffold(
+      body: child,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.home),
+            label: "navigation.home".tr(),
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.settings),
+            label: "navigation.settings".tr(),
+          )
+        ],
+        currentIndex: state.location == "/" ? 0 : 1,
+        onTap: (index) {
+          context.go(index == 0 ? "/" : "/settings");
+        },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    context.locale; // force rebuild when locale changes
+
+    final counter = ref.watch(counterProvider);
+
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -73,43 +262,92 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('home.title'.tr()),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: <Widget>[
+        Text('home.counter_text'.tr()),
+        Text('$counter').fontSize(32),
+      ].toColumn(mainAxisAlignment: MainAxisAlignment.center).center(),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () => ref.read(counterProvider.notifier).increment(),
+        tooltip: 'home.increment'.tr(),
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class MySettingsPage extends ConsumerWidget {
+  const MySettingsPage({super.key});
+
+  void _openAndroidAppLocaleSettings() {
+    const AndroidIntent(
+      action: 'android.settings.APP_LOCALE_SETTINGS',
+      data: 'package:fr.mrsquaare.tasktonic',
+    ).launch();
+  }
+
+  void _goToLanguageSettings(BuildContext context, bool perAppLocale) {
+    if (perAppLocale) {
+      _openAndroidAppLocaleSettings();
+    } else {
+      context.go("/settings/language");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    context.locale; // force rebuild when locale changes
+
+    final androidInfo = ref.read(androidInfoProvider).requireValue;
+    final perAppLocale = hasPerAppLocale(androidInfo);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('settings.title'.tr()),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.language),
+            title: Text('settings.languages'.tr()),
+            onTap: () {
+              _goToLanguageSettings(context, perAppLocale);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyLanguageSettingsPage extends StatelessWidget {
+  const MyLanguageSettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('settings.language.title'.tr()),
+      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: const Text('English'),
+            onTap: () {
+              context.setLocale(const Locale('en'));
+              context.pop();
+            },
+          ),
+          ListTile(
+            title: const Text('Français'),
+            onTap: () {
+              context.setLocale(const Locale('fr'));
+              context.pop();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,52 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tasktonic/main.dart';
 
-void main() {
+void main() async {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    tester.runAsync(() async {
+      await EasyLocalization.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(
+        MyAppWrappers(
+          child: MyApp(),
+        ),
+      );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+      expect(find.text('1'), findsNothing);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+      // Tap the '+' icon and trigger a frame.
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      // Verify that our counter has incremented.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    });
+  });
+
+  testWidgets('Counter state is not shared between tests', (tester) async {
+    tester.runAsync(() async {
+      await EasyLocalization.ensureInitialized();
+
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(
+        MyAppWrappers(
+          child: MyApp(),
+        ),
+      );
+
+      // Verify that our counter starts at 0.
+      expect(find.text('0'), findsNothing);
+      expect(find.text('1'), findsOneWidget);
+    });
   });
 }
